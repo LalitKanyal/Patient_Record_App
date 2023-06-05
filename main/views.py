f89ffcfc984102ed2de5e87710200fe71598a6f5
Lalit Kanyal <mailtolalit21@gmail.com>
2023-06-05 13:50:20 +0530

    create model form

--- main/views.py
+++ main/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from . models import Patient
+from . forms import PatientForm
+from django.contrib import messages
+
 
 def home(request):
     data=Patient.objects.all()
     return render(request, 'home.html',{'data':data})
+
+# Add Patient
+def addPatient(request):
+    form = PatientForm
+    if request.method == 'POST':
+        saveForm = PatientForm(request.POST)
+        if saveForm.is_valid():
+            saveForm.save()
+            messages.success(request, 'Data has been added successfully.')
+        return render(request, 'add-patient.html', {'form': form})
+    else:
+        return render(request, 'add-patient.html', {'form': form})
+
