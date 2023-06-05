c9b77badc596893e29980f837d6335c8fda0caf0
Lalit Kanyal <mailtolalit21@gmail.com>
2023-06-05 16:45:54 +0530

    complete

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ urlpatterns=[
     path('',views.home,name='home'),
     path('add-patient',views.addPatient,name='add-patient'),
     path('update-patient/<int:id>',views.updatePatient,name='update-patient'),
-    path('delete-patient/<int:id>',views.deletePatient,name='delete-patient')
+    path('delete-patient/<int:id>',views.deletePatient,name='delete-patient'),
+    path('send-email/<int:id>',views.sendEmail,name='send-email'),
+
 ]
